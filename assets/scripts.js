cf35b0344f57b3ff7107c26f169625eab529667b
CARCASSI Fausto <s1569804@ed.ac.uk>
2019-11-07 23:12:16 +0000

    Add page content dynamically

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,19 @@
+
 $(function() {
 
     $(".site-header").load("assets/header.html");
     $(".site-footer").load("assets/footer.html");
 
+    $("#home-link").click(function(){
+        $("#content-container").load("main.html")
+    });
+    $("#publications-link").click(function(){
+        $("#content-container").load("publications.html")
+    });
+    $("#teaching-link").click(function(){
+        $("#content-container").load("teaching.html")
+    });
+    $("#contact-link").click(function(){
+        $("#content-container").load("contact.html")
+    });
 });
