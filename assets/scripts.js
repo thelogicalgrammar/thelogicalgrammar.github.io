fbbf36a3e38922e28a0cc05749c41ccde429b61e
CARCASSI Fausto <s1569804@ed.ac.uk>
2019-11-07 23:27:20 +0000

    Update internal links

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,20 +1,22 @@
 
 $(function() {
 
-    $(".site-header").load("assets/header.html");
+    $(".site-header").load("assets/header.html", function(){
+        $("#home-link").click(function(){
+            console.log("Test");
+            $("#content-container").load("main.html")
+        });
+        $("#publications-link").click(function(){
+            $("#content-container").load("publications.html")
+        });
+        $("#teaching-link").click(function(){
+            $("#content-container").load("teaching.html")
+        });
+        $("#contact-link").click(function(){
+            $("#content-container").load("contact.html")
+        });
+    });
+
     $(".site-footer").load("assets/footer.html");
 
-    $("#home-link").click(function(){
-        console.log("Test");
-        $("#content-container").load("main.html")
-    });
-    $("#publications-link").click(function(){
-        $("#content-container").load("publications.html")
-    });
-    $("#teaching-link").click(function(){
-        $("#content-container").load("teaching.html")
-    });
-    $("#contact-link").click(function(){
-        $("#content-container").load("contact.html")
-    });
 });
